c6e23b1ab088e6defef4c316ca8e8ba2dd18b2fe
Silvia Patono <silvia.patono@gmail.com>
2020-02-09 21:26:32 +0100

    basic solution, no scss

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,13 @@
 class QuestionsController < ApplicationController
   def answer
+    @message = params['question']
+    if @message == 'I am going to work'
+      @answer = 'Great!'
+    elsif @message.strip[-1] == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 
   def ask
